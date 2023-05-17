ad9ec8bf9f5ed31a50dc9c02b4d1bf3d79585e9f
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-05-17 17:18:27 +0200

    operation functions

---
+++ monty_operations.c
@@ -0,0 +1,57 @@
+#include "monty.h"
+
+/**
+*push_element - The opcode push pushes an element to the stack
+*@stack: a double pointer
+*@line_number: an unsigned int
+*/
+
+void push_element(stack_t **stack, unsigned int line_number)
+{
+char *arg = NULL;
+
+arg = strtok(NULL, " \n\r\t");
+if (arg == NULL)
+{
+fprintf(stderr, "L%u: usage: push integer\n", line_number);
+exit(EXIT_FAILURE);
+}
+else
+{
+stack_t *node = malloc(sizeof(stack_t));
+
+if (!node)
+{
+fprintf(stderr, "Error: malloc failed\n");
+exit(EXIT_FAILURE);
+}
+node->n = atoi(arg);
+node->next = (*stack);
+node->prev = NULL;
+if (*stack)
+{
+(*stack)->prev = node;
+}
+*stack = node;
+}
+}
+
+/**
+*pall_elements - The opcode pall prints all the values on the stack
+*   starting from the top of the stack
+*@stack: a double pointer
+*@line_number: an unsigned integer
+*/
+
+void pall_elements(stack_t **stack, unsigned int line_number)
+{
+stack_t *node = *stack;
+
+(void)line_number; /* unused parameter */
+
+while (node != NULL)
+{
+printf("%d\n", node->n);
+node = node->next;
+}
+}
