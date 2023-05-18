50588286eba602fbd1e02667b6ea0837733381d7
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-05-18 12:39:55 +0200

    header file

--- monty.h
+++ monty.h
@@ -40,5 +40,6 @@ void push_element(stack_t **stack, unsigned int line_number);
 void pall_elements(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t **stack);
+void pop(stack_t **stack, unsigned int line_number);
 
 #endif
