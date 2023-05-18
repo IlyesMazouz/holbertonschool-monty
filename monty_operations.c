300b9d8ddb0459b0b25f23c3bae34e63328f03d0
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-05-18 12:14:37 +0200

    operation functions

--- monty_operations.c
+++ monty_operations.c
@@ -3,59 +3,59 @@
 #include "stdlib.h"
 
 /**
-*push_element - The opcode push pushes an element to the stack
-*@stack: a double pointer
-*@line_number: an unsigned int
-*/
+ *push_element - The opcode push pushes an element to the stack
+ *@stack: a double pointer
+ *@line_number: an unsigned int
+ */
 
 void push_element(stack_t **stack, unsigned int line_number)
 {
-char *arg = NULL;
+	char *arg = NULL;
 
-arg = strtok(NULL, " \n\r\t");
-if (arg == NULL)
-{
-fprintf(stderr, "L%u: usage: push integer\n", line_number);
-exit(EXIT_FAILURE);
-}
-else
-{
-stack_t *node = malloc(sizeof(stack_t));
+	arg = strtok(NULL, " \n\r\t");
+	if (arg == NULL)
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		stack_t *node = malloc(sizeof(stack_t));
 
-if (!node)
-{
-fprintf(stderr, "Error: malloc failed\n");
-exit(EXIT_FAILURE);
-}
-node->n = atoi(arg);
-node->next = (*stack);
-node->prev = NULL;
-if (*stack)
-{
-(*stack)->prev = node;
-}
-*stack = node;
-}
+		if (!node)
+		{
+			fprintf(stderr, "Error: malloc failed\n");
+			exit(EXIT_FAILURE);
+		}
+		node->n = atoi(arg);
+		node->next = (*stack);
+		node->prev = NULL;
+		if (*stack)
+		{
+			(*stack)->prev = node;
+		}
+		*stack = node;
+	}
 }
 
 /**
-*pall_elements - The opcode pall prints all the values on the stack
-*   starting from the top of the stack
-*@stack: a double pointer
-*@line_number: an unsigned integer
-*/
+ *pall_elements - The opcode pall prints all the values on the stack
+ *   starting from the top of the stack
+ *@stack: a double pointer
+ *@line_number: an unsigned integer
+ */
 
 void pall_elements(stack_t **stack, unsigned int line_number)
 {
-stack_t *node = *stack;
+	stack_t *node = *stack;
 
-(void)line_number; /* unused parameter */
+	(void)line_number; /* unused parameter */
 
-while (node != NULL)
-{
-printf("%d\n", node->n);
-node = node->next;
-}
+	while (node != NULL)
+	{
+		printf("%d\n", node->n);
+		node = node->next;
+	}
 }
 
 /**
@@ -66,10 +66,10 @@ node = node->next;
 
 void pint(stack_t **stack, unsigned int line_number)
 {
-if (*stack == NULL)
-{
-fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
-exit(EXIT_FAILURE);
-}
-printf("%d\n", (*stack)->n);
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
 }
